700d7de1dd432e6852972d1621bcdb16b20768ae
kpopse7en <yur.vargas@alumnos.duoc.cl>
2018-03-30 20:49:54 -0300

    :P

--- ClienteCollection/ClienteCollection.cs
+++ ClienteCollection/ClienteCollection.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace ClienteCollection
 {
-    class ClienteCollection
+    public class ClienteCollection
     {
+
+        //Crear una conexion
+        SqlConnection conexion;
+        String cadena=
+
     }
 }
