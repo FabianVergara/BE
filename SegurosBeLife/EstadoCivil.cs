4dd1acc9bb5152514aab2a85df6a43310256dde6
Fabian <vergarafabianandres@live.com>
2018-03-30 17:59:51 -0400

    probando

--- SegurosBeLife/EstadoCivil.cs
+++ SegurosBeLife/EstadoCivil.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace SegurosBeLife
 {
-    class EstadoCivil
-    {
+   public enum  EstadoCivil
+    { 
+        
     }
 }
--- SegurosBeLife/Sexo.cs
+++ SegurosBeLife/Sexo.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace SegurosBeLife
 {
-    class Sexo
+    public enum Sexo
     {
+        Feminino,Masculino,Otros
     }
 }
