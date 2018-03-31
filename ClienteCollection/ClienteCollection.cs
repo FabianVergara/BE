d08dbc2f82ca8d17f97a86cacf9e5c5885d5943f
Fabian <vergarafabianandres@live.com>
2018-03-31 16:15:23 -0400

    .

--- ClienteCollection/ClienteCollection.cs
+++ ClienteCollection/ClienteCollection.cs
@@ -14,7 +14,7 @@ namespace ClienteCollection
 
         //Crear una conexion
         SqlConnection conexion;
-        String cadena = @"Data Source = url;Initial Catalog=seguro;Persist Security Info=True;User ID=Sony-PC\Sony;Password=";
+        String cadena = @"Data Source = url;Initial Catalog=seguro;Persist Security Info=True;User ID=Sony-PC\Sony;Password=''";
         public ClienteCollection()
         {
             //instanciar conexion
